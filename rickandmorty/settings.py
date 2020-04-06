29c3eb1051ca536a7f6ffc66079ca2d6896e3325
ANDRES JAHR <asjahr@uc.cl>
2020-04-05 22:49:37 -0400

    Allow Heroku Host

--- rickandmorty/settings.py
+++ rickandmorty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o6se=f5xyu4151-k3qd9-eczthkqiz%5l+&7!j1xs+zsa)nzu&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []    
+ALLOWED_HOSTS = ['rickandmortyproject.herokuapp.com', '127.0.0.1']    
 
 
 # Application definition
